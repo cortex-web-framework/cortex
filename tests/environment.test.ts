998eaa5bea338da93ab3137f42ef1335c81b35ab
Matthias Kluth <kluthmatthias@gmail.com>
2025-10-20 10:07:04 +0200

    feat(setup): Configure node:test and remove jest

---
+++ tests/environment.test.ts
@@ -0,0 +1,6 @@
+import { test } from 'node:test';
+import assert from 'node:assert';
+
+test('Environment Setup: should run a test successfully', () => {
+  assert.strictEqual(true, true, 'Expected true to be true');
+});
